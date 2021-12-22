07d729e88fedc7d3a5c55918fd23198d34599c09
Rebekah Zhou <rebekahzhou@gmail.com>
2021-12-22 13:43:05 -0800

    completed assignemtn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat) {
+    return cats.push(cat);
+}
+
+function destructivelyPrependCat(cat) {
+    return cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(cat) {
+    const newCats = [...cats, cat]
+    return newCats;
+}
+
+function prependCat(cat) {
+    const newCats = [cat, ...cats]
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = [...cats.slice(0,-1)]
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [...cats.slice(1)]
+    return newCats;
+}
